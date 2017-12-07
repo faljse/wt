d130742e14565ee6496be0a87e1e30b2d50531f2
Martin Kunz <martin.michael.kunz@gmail.com>
2017-12-07 18:55:34 +0100

    i was ned

--- src/App.ts
+++ src/App.ts
@@ -2,12 +2,27 @@ import * as Koa from 'koa';
 import * as KoaBody from 'koa-body';
 import * as KoaRouter from 'koa-router';
 import * as KoaSend from 'koa-send';
+import * as Faker from 'faker';
+import * as Nodemailer from 'nodemailer';
 
 import { MyLogger } from "./mylogger";
 const log = MyLogger.create("App");
 
+interface ManualTaskData {
+  role: string;
+  organisation: string;
+  link: string;
+}
+
 // Creates and configures an ExpressJS web server.
 export class App {
+  // create reusable transporter object using the default SMTP transport
+  private transporter = Nodemailer.createTransport({
+    host: 'mail.univie.ac.at',
+    port: 25,
+    secure: false, // true for 465, false for other ports
+  });
+
   private koa = new Koa();
   //Run configuration methods on the Express instance.
   constructor(private port) {
@@ -29,80 +44,219 @@ export class App {
       await KoaSend(ctx, '/static/index.html');
       next();
     });
+
+    //manual: customerSendsOrder
     router.get('/customerSendsOrder', async (ctx, next) => {
       log.info(ctx.path);
       await KoaSend(ctx, '/static/customerSendsOrder.html');
       next();
     });
     router.post('/customerSendsOrder', KoaBody(), async (ctx, next) => {
-      ctx.body = JSON.stringify(ctx.request.body);    
-      ctx.type = 'application/json';
+      let data: ManualTaskData = ctx.request.body;
+      log.info("role: %s", data.role);
+      ctx.body = Faker.fake("{{name.lastName}}, {{name.firstName}} {{name.suffix}}")
+      // ctx.type = 'application/json';
       next();
     });
+
+    //manual: customerSendsSpecification
     router.get('/customerSendsSpecification', async (ctx, next) => {
-      await KoaSend(ctx, '/static/customerSendsOrder.html');      
+      await KoaSend(ctx, '/static/customerSendsSpecification.html');
       next();
     });
+    router.post('/customerSendsSpecification', KoaBody(), async (ctx, next) => {
+      let data: ManualTaskData = ctx.request.body;
+      // ctx.type = 'application/json';      
+      ctx.body = Faker.random.arrayElement(["VodkaBar", "WhiskeyBar", "BeerBar", "SakeBar", "TequillaBar"]);
+      next();
+    });
+
+    //auto: buildBaseModel
     router.get('/buildBaseModel', async (ctx, next) => {
-      await KoaSend(ctx, '/static/buildBaseModel.html');      
+      await KoaSend(ctx, '/static/buildBaseModel.html');
       next();
     });
-    router.get('/orderSeats', async (ctx, next) => {
-      await KoaSend(ctx, '/static/orderSeats.html');      
+    router.post('/buildBaseModel', KoaBody(), async (ctx, next) => {
+      log.info("buildBaseModel: %s", ctx.request.body);
+      let amount: number = ctx.request.body['amount'];
+      //do something
+      ctx.body = {success: {}};
       next();
     });
-    router.get('/orderToilet', async (ctx, next) => {
-      await KoaSend(ctx, '/static/orderToilet.html');      
+
+    //auto: orderSeats
+    router.post('/orderSeats', KoaBody(), async (ctx, next) => {
+      let amount: number = ctx.request.body['amount'];
+      let from: string = ctx.request.body['from'];
+      let notify: string = ctx.request.body['notify'];
+      let mailOptions = {
+        from: from, to: notify,
+        subject: 'Order Seats!', text: 'Amount:' + amount
+      };
+      this.transporter.sendMail(mailOptions, (error, info) => {
+        if (error) { return log.warn(error.message); }
+      });
+      ctx.body = {success: {}};
       next();
     });
-    router.get('/orderVodkaBar', async (ctx, next) => {
-      await KoaSend(ctx, '/static/orderVodkaBar.html');      
+
+    //auto: orderToilet
+    router.post('/orderToilet', KoaBody(), async (ctx, next) => {
+      let amount: number = ctx.request.body['amount'];
+      let from: string = ctx.request.body['from'];
+      let notify: string = ctx.request.body['notify'];
+      let mailOptions = {
+        from: from, to: notify,
+        subject: 'Order Toilet!', text: 'Toilet Amount:' + amount
+      };
+      this.transporter.sendMail(mailOptions, (error, info) => {
+        if (error) { return log.warn(error.message); }
+      });
+      ctx.body = {success: {}};
       next();
     });
-    router.get('/orderWhiskeyBar', async (ctx, next) => {
-      await KoaSend(ctx, '/static/orderWhiskeyBar.html');      
+
+    //auto: orderVodkaBar
+    router.post('/orderVodkaBar', KoaBody(), async (ctx, next) => {
+      let amount: number = ctx.request.body['amount'];
+      let from: string = ctx.request.body['from'];
+      let notify: string = ctx.request.body['notify'];
+      let mailOptions = {
+        from: from, to: notify,
+        subject: 'Order VodkaBar!', text: 'VodkaBar Amount:' + amount
+      };
+      this.transporter.sendMail(mailOptions, (error, info) => {
+        if (error) { return log.warn(error.message); }
+      });
+      ctx.body = {success: {}};
       next();
     });
-    router.get('/orderTequillaBar', async (ctx, next) => {
-      await KoaSend(ctx, '/static/orderTequillaBar.html');      
+
+    //auto: orderWhiskeyBar
+    router.post('/orderWhiskeyBar', KoaBody(), async (ctx, next) => {
+      let amount: number = ctx.request.body['amount'];
+      let from: string = ctx.request.body['from'];
+      let notify: string = ctx.request.body['notify'];
+      let mailOptions = {
+        from: from, to: notify,
+        subject: 'Order WhiskeyBar!', text: 'WhiskeyBar Amount:' + amount
+      };
+      this.transporter.sendMail(mailOptions, (error, info) => {
+        if (error) { return log.warn(error.message); }
+      });
+      ctx.body = {success: {}};
       next();
     });
-    router.get('/orderBeerBar', async (ctx, next) => {
-      await KoaSend(ctx, '/static/orderBeerBar.html');      
+
+    //auto: orderTequillaBar
+    router.post('/orderTequillaBar', KoaBody(), async (ctx, next) => {
+      let amount: number = ctx.request.body['amount'];
+      let from: string = ctx.request.body['from'];
+      let notify: string = ctx.request.body['notify'];
+      let mailOptions = {
+        from: from, to: notify,
+        subject: 'Order TequillaBar!', text: 'TequillaBar Amount:' + amount
+      };
+      this.transporter.sendMail(mailOptions, (error, info) => {
+        if (error) { return log.warn(error.message); }
+      });
+      ctx.body = {success: {}};
       next();
     });
-    router.get('/orderSakeBar', async (ctx, next) => {
-      await KoaSend(ctx, '/static/orderBeerBar.html');      
+
+    //auto: orderBeerBar
+    router.post('/orderBeerBar', KoaBody(), async (ctx, next) => {
+      let amount: number = ctx.request.body['amount'];
+      let from: string = ctx.request.body['from'];
+      let notify: string = ctx.request.body['notify'];
+      let mailOptions = {
+        from: from, to: notify,
+        subject: 'Order BeerBar!', text: 'BeerBar Amount:' + amount
+      };
+      this.transporter.sendMail(mailOptions, (error, info) => {
+        if (error) { return log.warn(error.message); }
+      });
+      ctx.body = {success: {}};
       next();
     });
-    router.get('/waitForParts', async (ctx, next) => {
-      await KoaSend(ctx, '/static/orderBeerBar.html');      
+
+    //auto: orderSakeBar
+    router.post('/orderSakeBar', KoaBody(), async (ctx, next) => {
+      let amount: number = ctx.request.body['amount'];
+      let from: string = ctx.request.body['from'];
+      let notify: string = ctx.request.body['notify'];
+      let mailOptions = {
+        from: from, to: notify,
+        subject: 'Order BeerBar!', text: 'BeerBar Amount:' + amount
+      };
+      this.transporter.sendMail(mailOptions, (error, info) => {
+        if (error) { return log.warn(error.message); }
+      });
+      ctx.body = {success: {}};
       next();
     });
-    router.get('/assemblePlane', async (ctx, next) => {
-      await KoaSend(ctx, '/static/orderBeerBar.html');      
+
+    //auto: waitForParts
+    router.post('/waitForParts', KoaBody(), async (ctx, next) => {      
+      ctx.body = {success: {}};
       next();
     });
+
+    //auto: assemblePlane
+    router.post('/assemblePlane', async (ctx, next) => {
+      ctx.body = {success: {}};
+      next();
+    });
+
+    //manual: testFlight
     router.get('/testFlight', async (ctx, next) => {
-      await KoaSend(ctx, '/static/orderBeerBar.html');      
+      await KoaSend(ctx, '/static/testFlight.html');
       next();
     });
+    router.post('/testFlight', async (ctx, next) => {
+      ctx.body=Faker.random.number({min:100,max:3600});
+      next();
+    });
+
+    //manual: evaluateFlight
     router.get('/evaluateFlight', async (ctx, next) => {
-      await KoaSend(ctx, '/static/evaluateFlight.html');      
+      await KoaSend(ctx, '/static/evaluateFlight.html');
       next();
     });
+    router.post('/evaluateFlight', async (ctx, next) => {
+      ctx.body = Faker.random.arrayElement(["true", "false"]);
+      next();
+    });
+
+    //manual: repair
     router.get('/repair', async (ctx, next) => {
-      await KoaSend(ctx, '/static/repair.html');      
+      await KoaSend(ctx, '/static/repair.html');
       next();
     });
+    router.post('/repair', async (ctx, next) => {
+      ctx.body = Faker.random.arrayElement(["body", "avionics", "radio", "engine"]);
+      next();
+    });
+
+    //auto: deliverPlane
     router.get('/deliverPlane', async (ctx, next) => {
-      await KoaSend(ctx, '/static/deliverPlane.html');      
+      await KoaSend(ctx, '/static/deliverPlane.html');
       next();
     });
+    router.post('/deliverPlane', async (ctx, next) => {
+      ctx.body = Faker.random.arrayElement(["true", "false"]);
+      next();
+    });
+
+    //auto: receiveConfirmation
     router.get('/receiveConfirmation', async (ctx, next) => {
-      await KoaSend(ctx, '/static/receiveConfirmation.html');      
+      await KoaSend(ctx, '/static/receiveConfirmation.html');
       next();
     });
+    router.post('/receiveConfirmation', async (ctx, next) => {
+      next();
+    });
+
     router.get('/static/*', async (ctx, next) => {
       log.info(ctx.path);
       await KoaSend(ctx, ctx.path);
