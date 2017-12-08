fd788004dffee6f6ccf4dbe699fe4884f2d2f304
Martin Kunz <martin.michael.kunz@gmail.com>
2017-12-08 12:51:02 +0100

    wwi

--- src/App.ts
+++ src/App.ts
@@ -54,8 +54,10 @@ export class App {
     router.post('/customerSendsOrder', KoaBody(), async (ctx, next) => {
       let data: ManualTaskData = ctx.request.body;
       log.info("role: %s", data.role);
-      ctx.body = Faker.fake("{{name.lastName}}, {{name.firstName}} {{name.suffix}}")
-      // ctx.type = 'application/json';
+      ctx.type = 'application/json';
+      ctx.body = JSON.stringify(ctx.body = {
+        name: Faker.fake("{{name.lastName}}, {{name.firstName}} {{name.suffix}}")
+      });
       next();
     });
 
@@ -65,9 +67,12 @@ export class App {
       next();
     });
     router.post('/customerSendsSpecification', KoaBody(), async (ctx, next) => {
+      log.info("customerSendsSpecification");
       let data: ManualTaskData = ctx.request.body;
-      // ctx.type = 'application/json';      
-      ctx.body = Faker.random.arrayElement(["VodkaBar", "WhiskeyBar", "BeerBar", "SakeBar", "TequillaBar"]);
+      ctx.type = 'application/json';      
+      ctx.body = JSON.stringify({
+        barType: Faker.random.arrayElement(["VodkaBar", "WhiskeyBar", "BeerBar", "SakeBar", "TequillaBar"])
+      });
       next();
     });
 
@@ -79,14 +84,17 @@ export class App {
     router.post('/buildBaseModel', KoaBody(), async (ctx, next) => {
       log.info("buildBaseModel: %s", ctx.request.body);
       let amount: number = ctx.request.body['amount'];
-      //do something
-      ctx.body = {success: {}};
+      ctx.type = 'application/json';      
+      ctx.body = JSON.stringify({
+        build: "success"
+      })
       next();
     });
 
     //auto: orderSeats
     router.post('/orderSeats', KoaBody(), async (ctx, next) => {
       let amount: number = ctx.request.body['amount'];
+      log.info("orderSeats amount: %d",amount);
       let from: string = ctx.request.body['from'];
       let notify: string = ctx.request.body['notify'];
       let mailOptions = {
@@ -96,13 +104,15 @@ export class App {
       this.transporter.sendMail(mailOptions, (error, info) => {
         if (error) { return log.warn(error.message); }
       });
-      ctx.body = {success: {}};
+      ctx.type = 'application/json';
+      ctx.body = JSON.stringify({success: {notified:notify}});
       next();
     });
 
     //auto: orderToilet
     router.post('/orderToilet', KoaBody(), async (ctx, next) => {
       let amount: number = ctx.request.body['amount'];
+      log.info("orderToilet amount: %d",amount);
       let from: string = ctx.request.body['from'];
       let notify: string = ctx.request.body['notify'];
       let mailOptions = {
@@ -112,13 +122,15 @@ export class App {
       this.transporter.sendMail(mailOptions, (error, info) => {
         if (error) { return log.warn(error.message); }
       });
-      ctx.body = {success: {}};
+      ctx.type = 'application/json';
+      ctx.body = JSON.stringify({success: {notified:notify}});
       next();
     });
 
     //auto: orderVodkaBar
     router.post('/orderVodkaBar', KoaBody(), async (ctx, next) => {
       let amount: number = ctx.request.body['amount'];
+      log.info("orderVodkaBar");
       let from: string = ctx.request.body['from'];
       let notify: string = ctx.request.body['notify'];
       let mailOptions = {
@@ -128,12 +140,14 @@ export class App {
       this.transporter.sendMail(mailOptions, (error, info) => {
         if (error) { return log.warn(error.message); }
       });
-      ctx.body = {success: {}};
+      ctx.type = 'application/json';
+      ctx.body = JSON.stringify({success: {notified:notify}});
       next();
     });
 
     //auto: orderWhiskeyBar
     router.post('/orderWhiskeyBar', KoaBody(), async (ctx, next) => {
+      log.info("orderWhiskeyBar");
       let amount: number = ctx.request.body['amount'];
       let from: string = ctx.request.body['from'];
       let notify: string = ctx.request.body['notify'];
@@ -144,12 +158,14 @@ export class App {
       this.transporter.sendMail(mailOptions, (error, info) => {
         if (error) { return log.warn(error.message); }
       });
-      ctx.body = {success: {}};
+      ctx.type = 'application/json';
+      ctx.body = JSON.stringify({success: {notified:notify}});
       next();
     });
 
     //auto: orderTequillaBar
     router.post('/orderTequillaBar', KoaBody(), async (ctx, next) => {
+      log.info("orderTequillaBar");
       let amount: number = ctx.request.body['amount'];
       let from: string = ctx.request.body['from'];
       let notify: string = ctx.request.body['notify'];
@@ -160,12 +176,14 @@ export class App {
       this.transporter.sendMail(mailOptions, (error, info) => {
         if (error) { return log.warn(error.message); }
       });
-      ctx.body = {success: {}};
+      ctx.type = 'application/json';
+      ctx.body = JSON.stringify({success: {notified:notify}});
       next();
     });
 
     //auto: orderBeerBar
     router.post('/orderBeerBar', KoaBody(), async (ctx, next) => {
+      log.info("orderBeerBar");
       let amount: number = ctx.request.body['amount'];
       let from: string = ctx.request.body['from'];
       let notify: string = ctx.request.body['notify'];
@@ -176,35 +194,44 @@ export class App {
       this.transporter.sendMail(mailOptions, (error, info) => {
         if (error) { return log.warn(error.message); }
       });
-      ctx.body = {success: {}};
+      ctx.type = 'application/json';
+      ctx.body = JSON.stringify({success: {notified:notify}});
       next();
     });
 
     //auto: orderSakeBar
     router.post('/orderSakeBar', KoaBody(), async (ctx, next) => {
+      log.info("orderSakeBar");
       let amount: number = ctx.request.body['amount'];
       let from: string = ctx.request.body['from'];
       let notify: string = ctx.request.body['notify'];
       let mailOptions = {
         from: from, to: notify,
-        subject: 'Order BeerBar!', text: 'BeerBar Amount:' + amount
+        subject: 'Order SakeBar!', text: 'SakeBar Amount:' + amount
       };
       this.transporter.sendMail(mailOptions, (error, info) => {
         if (error) { return log.warn(error.message); }
       });
-      ctx.body = {success: {}};
+      ctx.type = 'application/json';
+      ctx.body = JSON.stringify({success: {notified:notify}});
       next();
     });
 
     //auto: waitForParts
-    router.post('/waitForParts', KoaBody(), async (ctx, next) => {      
-      ctx.body = {success: {}};
+    router.post('/waitForParts', KoaBody(), async (ctx, next) => { 
+      log.info("waitForParts");
+      await this.sleep(4000);
+      ctx.type = 'application/json';
+      ctx.body = JSON.stringify({success: {arrived:true}});
       next();
     });
 
     //auto: assemblePlane
-    router.post('/assemblePlane', async (ctx, next) => {
-      ctx.body = {success: {}};
+    router.post('/assemblePlane', KoaBody(), async (ctx, next) => {
+      log.info("assemblePlane");
+      await this.sleep(4000);
+      ctx.type = 'application/json';
+      ctx.body = JSON.stringify({success: {ready:true}});
       next();
     });
 
@@ -213,8 +240,10 @@ export class App {
       await KoaSend(ctx, '/static/testFlight.html');
       next();
     });
-    router.post('/testFlight', async (ctx, next) => {
-      ctx.body=Faker.random.number({min:100,max:3600});
+    router.post('/testFlight', KoaBody(), async (ctx, next) => {
+      ctx.type = 'application/json';
+      ctx.body= JSON.stringify({duration: Faker.random.number({min:100,max:3600})});
+      log.info("testFlight %s", ctx.body);    
       next();
     });
 
@@ -223,18 +252,22 @@ export class App {
       await KoaSend(ctx, '/static/evaluateFlight.html');
       next();
     });
-    router.post('/evaluateFlight', async (ctx, next) => {
-      ctx.body = Faker.random.arrayElement(["true", "false"]);
+    router.post('/evaluateFlight', KoaBody(), async (ctx, next) => {
+      ctx.type = 'application/json';
+      let result=JSON.stringify({survived: Faker.random.arrayElement(["true", "false"])});
+      log.info("evaluateflight: %s", result);
+      ctx.body = result;
       next();
     });
 
     //manual: repair
     router.get('/repair', async (ctx, next) => {
       await KoaSend(ctx, '/static/repair.html');
+      log.info("repair");      
       next();
     });
-    router.post('/repair', async (ctx, next) => {
-      ctx.body = Faker.random.arrayElement(["body", "avionics", "radio", "engine"]);
+    router.post('/repair', KoaBody(), async (ctx, next) => {
+      ctx.body = JSON.stringify({repaired: Faker.random.arrayElement(["body", "avionics", "radio", "engine"])});
       next();
     });
 
@@ -243,8 +276,18 @@ export class App {
       await KoaSend(ctx, '/static/deliverPlane.html');
       next();
     });
-    router.post('/deliverPlane', async (ctx, next) => {
-      ctx.body = Faker.random.arrayElement(["true", "false"]);
+    router.post('/deliverPlane', KoaBody(), async (ctx, next) => {
+      let from: string = ctx.request.body['from'];
+      let notify: string = ctx.request.body['notify'];
+      let mailOptions = {
+        from: from, to: notify,
+        subject: 'Plane invoice', text: 'Customers invoice'
+      };
+      this.transporter.sendMail(mailOptions, (error, info) => {
+        if (error) { return log.warn(error.message); }
+      });
+
+      ctx.body = JSON.stringify({delivered: Faker.random.arrayElement(["true", "false"])});
       next();
     });
 
@@ -253,7 +296,9 @@ export class App {
       await KoaSend(ctx, '/static/receiveConfirmation.html');
       next();
     });
-    router.post('/receiveConfirmation', async (ctx, next) => {
+    router.post('/receiveConfirmation', KoaBody(), async (ctx, next) => {
+      await this.sleep(2000); //wait for confirmation
+      ctx.body = JSON.stringify({confirmed: Faker.random.arrayElement(["true", "false"])});
       next();
     });
 
@@ -290,4 +335,8 @@ export class App {
       log.info(`Listening on ${bind}`);
     }
   }
+
+  sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+  
+
 }
